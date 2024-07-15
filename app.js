a376faef4b3859d9fe0425170c1807146d160cd0
Kevin Schlundt <web.kevin@hotmail.com>
2024-07-15 15:51:23 -0300

    titulo y parrafo añadido

---
+++ app.js
@@ -0,0 +1,8 @@
+let titulo = document.querySelector('h1');
+let parrafo = document.querySelector('p');
+titulo.textContent = 'Juego del número secreto';
+parrafo.innerHTML = 'Indica un número del 1 al 10';
+
+function intentoDeUSuario(){
+    
+}
