cff69615929e7d4aaf2a7d5234b4890b65dcaace
Kevin Schlundt <web.kevin@hotmail.com>
2024-07-16 23:24:39 -0300

    Funcion AsignarTextoElemento añadida

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let titulo = document.querySelector('h1');
+
 let parrafo = document.querySelector('p');
-titulo.textContent = 'Juego del número secreto';
 parrafo.innerHTML = 'Indica un número del 1 al 10';
 
-function intentoDeUSuario(){
-    
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
 }
+
+function intentoDeUsuario(){
+    console.log("Click");
+}
+asignarTextoElemento('h1','juego del numero secreto');
