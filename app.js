fa97076577c291bcc260c85cd9c274b481bc99fc
Kevin Schlundt <web.kevin@hotmail.com>
2024-07-17 19:36:46 -0300

    funcion generarNumeroSecreto añadida

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un número del 1 al 10';
@@ -5,9 +7,19 @@ parrafo.innerHTML = 'Indica un número del 1 al 10';
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function intentoDeUsuario(){
     console.log("Click");
+    return;
 }
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+    
+    
+}
+
 asignarTextoElemento('h1','juego del numero secreto');
+asignarTextoElemento('p','Indica un numero del 1 al 10');
