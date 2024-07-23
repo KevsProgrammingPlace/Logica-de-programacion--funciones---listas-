c5ba7a0af4251b0773cee7694ce9e12821fe203e
Kevin Schlundt <web.kevin@hotmail.com>
2024-07-22 23:25:57 -0300

    ejercicios realizados

--- ejercicios/app.js
+++ ejercicios/app.js
@@ -30,10 +30,24 @@ function promedioNumeros(num,num2,num3){
 //====================================================================================================================//
 //EJERCICIO12
 //Crear una función que reciba dos números como parámetros y devuelva el mayor de ellos.
-
+function elMayorDeTodos(num1,num2){
+    if (num1 > num2){
+        console.log('el numero '+num1+' es mayor que ' + num2)
+    }else{
+        console.log('el numero '+num2+' es mayor que '+num1)
+    }
+}
+//====================================================================================================================//
+//EJERCICIO13
+//Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo.
+function multiplicarNumero(num1){
+    console.log(num1*num1);
+}
 
 //====================================================================================================================//
 helloWorld();
 saludarPersona("Kevin");
 multiplicarNumero(10);
 promedioNumeros(9,10,15);
+elMayorDeTodos(20,20)
+multiplicarNumero(9);
